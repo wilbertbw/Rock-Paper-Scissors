4a1db9c535c79539166b7e7043b1223a3663fec9
wilbertbw <81511269+wilbertbw@users.noreply.github.com>
2023-07-14 20:51:41 +0700

    Update rock-paper-scissors-auto.js

--- rock-paper-scissors-auto.js
+++ rock-paper-scissors-auto.js
@@ -6,6 +6,18 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
 updateScoreElement();
 
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
 let result = '';
 
 function pickComputerMove() {
